3d78cea1523bc10b2216c6b1ea16ccaa58844b99
marcodiazz <marcopadel21@gmail.com>
2023-04-18 21:39:48 +0200

    new graphs

--- ventanas/apache.js
+++ ventanas/apache.js
@@ -2,8 +2,6 @@ let xValuesGravedad = ["Grave", "Moderado", "Estable"];
 let yValues = [15, 30, 55];
 let barColors = ["#BD0909", "#EB9617", "#1CA44A"];
 
-const ctx1 = document.getElementById('chart1')
-const ctx2 = document.getElementById('chart2')
 
 new Chart("chart1", {
     type: "bar",
@@ -17,6 +15,7 @@ new Chart("chart1", {
     options: {
     }
   });
+
   new Chart("chart2", {
     type: "doughnut",
     data: {
@@ -37,5 +36,33 @@ new Chart("chart1", {
         }]
       },
     options: {
+        aspectRatio: 1.5
+    }
+  });
+
+  new Chart("chart3", {
+    type: "polarArea",
+    data: {
+        labels: [
+          'Red',
+          'Green',
+          'Yellow',
+          'Grey',
+          'Blue'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [11, 16, 7, 3, 14],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+            'rgb(201, 203, 207)',
+            'rgb(54, 162, 235)'
+          ]
+        }]
+    },
+    options: {
+            aspectRatio: 1.5
     }
   });
